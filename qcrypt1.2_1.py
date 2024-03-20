9e8d4190e450210cada851bdfa255a32f6818794
Glo_ <149516588+GloAlgara@users.noreply.github.com>
2024-03-20 15:23:55 +0100

    Update qcrypt1.2_1.py

--- qcrypt1.2_1.py
+++ qcrypt1.2_1.py
@@ -7,7 +7,7 @@ psi = np.array([1/np.sqrt(2), np.exp(1j*np.pi/4)/np.sqrt(2)])
 density_matrix = np.outer(psi, np.conj(psi))
 
 print("|ψ⟩⟨ψ|:")
-print(density_matrix)
+print(np.real(density_matrix))
 
 X_gate = np.array([[0, 1], [1, 0]])
 Y_gate = np.array([[0, -1j], [1j, 0]])
@@ -20,18 +20,18 @@ Z_result = np.real(np.dot(Z_gate, density_matrix))
 
 print()
 print("X*ψ:")
-print(X_result)
+print(np.real(X_result))
 print()
 print("Y*ψ:")
 print(Y_result)
 print()
 print("Z*ψ:")
-print(Z_result)
+print(np.real(Z_result))
 
 print()
 print("Calculate the coefficients: ")
-print("X: ", X_result[0][0] + X_result[1][1])
-print("Y: ", Y_result[0][0] + Y_result[1][1])
+print("X: ", np.real(X_result[0][0] + X_result[1][1]))
+print("Y: ", np.real(Y_result[0][0] + Y_result[1][1]))
 print("Z: ", np.round(Z_result[0][0]) + np.round(Z_result[1][1]))
 
 
